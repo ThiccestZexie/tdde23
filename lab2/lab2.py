f8e6d86c70ea7de645d0e76506be7a019e7c5dca
Daniel Alchasov <danal315@student.liu.se>
2022-09-11 21:23:38 +0000

    Replace lab2.py

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,4 +1,33 @@
-# Här kan du skriva programkod för labb 2.
-# Du behöver strikt sett inte lämna in koden för denna labb,
-# men vi lägger ändå in en förberedd fil för att förenkla.
-# Om du har skrivit labbkoden någon annanstans är det också OK.
+import math
+
+def check_pnr(checkPnr):
+    viktPrn = checkPnr.copy()
+    vProdukter(viktPrn)
+    sum = vSumma(viktPrn)
+    sum -= checkPnr[9]
+    rounded = math.ceil((sum/10)) * 10
+    Rest = rounded - sum  # kontroll
+    return bool(Rest == checkPnr[9])
+   
+
+def vProdukter(viktPrn):
+        for i in range(len(viktPrn)-1):
+            if (i + 1) % 2 == 0:
+                viktPrn[i] = viktPrn[i] * 1
+            else:
+                viktPrn[i] = viktPrn[i] * 2
+
+def vSumma(viktPro):
+    sOfPrnmr = '' .join(map(str, viktPro)) # make list into string
+    sum = 0
+    for siffror in str(sOfPrnmr): 
+        sum += int(siffror)
+    
+    
+    return sum
+ 
+print(check_pnr([7, 4, 0, 2, 1, 7, 4, 8, 2, 0]))
+
+
+
+

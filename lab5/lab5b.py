77645aa302216acdbbc4d3e2cef6d21e26ae2683
Daniel Alchasov <danal315@student.liu.se>
2022-11-01 19:56:00 +0000

    Replace lab5b.py

--- lab5/lab5b.py
+++ lab5/lab5b.py
@@ -8,12 +8,22 @@ import numpy
 
 def pixel_constraint(hlow, hhigh, slow, shigh, vlow, vhigh):
 
+<<<<<<< HEAD
+    """Checks if a pixels HSV values matches with a set conditions and returns 1 or 0 depending if it does or not"""
+
+    def pixel_condition(hsv):
+=======
     def is_what(hsv):
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
         if vlow <= hsv[2] <= vhigh and hlow <= hsv[0] <= hhigh and slow <= hsv[1] <= shigh:
             return 1
         else:
             return 0
+<<<<<<< HEAD
+    return pixel_condition
+=======
     return is_what
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
 
 
 def multiply_tuple(tpl, mult):
@@ -35,6 +45,12 @@ def add_tuples(tpl1, tpl2):
 
 
 def cvimg_to_list(filename):
+<<<<<<< HEAD
+
+    """Returns a list of tuples of a images RGB values"""
+
+=======
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
     list_of_colors = []
     image =  filename
     for x in range(image.shape[0]):
@@ -60,13 +76,25 @@ def rgblist_to_cvimg(lst, height, width):
 
 
 def generator_from_image(image_as_list):
+<<<<<<< HEAD
+    """Gives BGR value for a pixel after it reads the list of it given a index"""
+=======
 
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
     def generator(index):
         return image_as_list[index]
     return generator
 
  
+<<<<<<< HEAD
+def combine_images(mask, mask_function, image_generator1, image_generator2):  
+    """Combine 2 based on a mask with the help of a mask function that decides if img 1 or img 2 should be used or a blend 
+    
+    Returns a list of the BGR values for each pixel for the new image
+    """
+=======
 def combine_images(mask, mask_function, image_generator1, image_generator2):  # just brute force it ig
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
     list_colors = []
     img1 = [image_generator1(i) for i in range(len(mask))]
     img2 = [image_generator2(i) for i in range(len(mask))]
@@ -76,7 +104,11 @@ def combine_images(mask, mask_function, image_generator1, image_generator2):  # 
         elif mask_function(mask[A]) == 0:
             list_colors.append(img2[A])
         else:
+<<<<<<< HEAD
+            list_colors.append(add_tuples(multiply_tuple(img1[A], condition(mask[A])), multiply_tuple(img2[A], (1-condition(mask[A])))))
+=======
             list_colors.append(add_tuples(multiply_tuple(img1[A], condition(A)), multiply_tuple(img2[A], (1-condition(A)))))
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
     return list_colors
 
 
@@ -92,6 +124,18 @@ def greyscale_list_to_cvimg(lst, height, width):
  
 
 def gradient_condition(mask):
+<<<<<<< HEAD
+    """Retuns a value for a pixel dependent on RGB value when they all are the same."""
+    def condition(index):
+        if isinstance(index, tuple):
+            (r,g,b) = index
+            if r == g == b:
+                return r/255
+        else:
+            return r/255
+        
+    return condition
+=======
     
     def condition(index):
         return round((sum(mask[index])/765), 2) 
@@ -124,3 +168,4 @@ result = combine_images(mask, condition, generator1, generator2)
 new_img = rgblist_to_cvimg(result, plane_img.shape[0], plane_img.shape[1])
 cv2.imshow('Final image', new_img)
 cv2.waitKey(0)
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6

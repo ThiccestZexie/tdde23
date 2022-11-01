e3ac4aea49e7e85dcc04ce689700774c474e3071
Daniel Alchasov <danal315@student.liu.se>
2022-11-01 20:04:24 +0000

    Replace lab3.py

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -2,58 +2,74 @@ import math
 
 def new_board():
     return {}
-def is_free(gameBoard, y, x):
-    if(y,x) in gameBoard:
+
+
+def is_free(gameBoard, x, y):
+    if(x,y) in gameBoard:
         return False
     else:
         return True
-def place_piece(gameBoard, y, x, piece):
-    if is_free(gameBoard, y, x):
-        gameBoard[(y,x)] = piece
+
+
+def place_piece(gameBoard, x, y, piece):
+    if is_free(gameBoard, x, y):
+        gameBoard[(x,y)] = piece
         return True
     else:
         return False
-def remove_piece(gameBoard, y, x):
-    if not is_free(gameBoard, y, x):
-        del gameBoard[(y,x)]
+
+
+def remove_piece(gameBoard, x, y):
+    if not is_free(gameBoard, x, y):
+        del gameBoard[(x,y)]
         return True
     else:
         return False
-def move_piece(gameBoard, y, x, newY, newX):
-    if is_free(gameBoard, newY, newX):
-        name = gameBoard[(y,x)]
-        remove_piece(gameBoard, y,x)
-        place_piece(gameBoard, newY, newX, name)
+
+
+def move_piece(gameBoard, x, y, newx, newy):
+    if is_free(gameBoard, newx, newy): # kollar inte x,y innan den bxter plats
+        piece = gameBoard[(x,y)]
+        remove_piece(gameBoard, x,y)
+        place_piece(gameBoard, newx, newy, piece)
         return True
     else:
         return False
-def get_piece(gameBoard, y, x):
-    if not is_free(gameBoard,y, x):
-        return gameBoard.get((y,x))
+
+
+def get_piece(gameBoard, x, y):
+    if not is_free(gameBoard,x, y):
+        return gameBoard.get((x,y))
     else:
         return False
-def nearest_piece(gameBoard, y, x):
+
+
+def nearest_piece(gameBoard, x, y):
     newDis = 0
     lowDis = 0
     lowestCord = ()
+    
     for key in gameBoard:
-        newDis = math.dist((y,x), key)
+        newDis = math.dist((x,y), key)
         if lowDis > newDis:
             lowDis = newDis
             lowestCord = key
         else:
           lowDis = newDis
     return lowestCord
-def count(gameBoard, type, cord, name):
+
+
+def count(gameBoard, txpe, cord, piece):
     sum = 0
-    if type == "row":
+    if txpe == "row":
         for keys in gameBoard: 
-            if gameBoard[keys] == name:
+            if gameBoard[keys] == piece:
                 if keys[1] == cord:
                     sum += 1
-    if type == "column":
+    if txpe == "column":
        for keys in gameBoard: 
-            if gameBoard[keys] == name:
+            if gameBoard[keys] == piece:
                 if keys[0] == cord:
                     sum += 1
     return sum
+

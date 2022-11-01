89349a3708a21fee3bb2c18547c149606be19b5b
Daniel Alchasov <danal315@student.liu.se>
2022-11-01 20:11:36 +0000

    Replace lab4b.py

--- lab4/lab4b.py
+++ lab4/lab4b.py
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+def interpret(LogicalExpression, Interpretation):
+=======
 from re import I
 
 
@@ -5,10 +8,31 @@ def interpret(LogicalExpression, Interpretation):
 
     if len(Interpretation) == 0:
             return 'true'
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
     if LogicalExpression == 'true':
         return LogicalExpression
     if LogicalExpression == 'false':
         return LogicalExpression
+<<<<<<< HEAD
+    if isinstance(LogicalExpression, str):
+        if LogicalExpression in Interpretation:
+            return Interpretation[LogicalExpression]
+        return LogicalExpression
+
+    if len(LogicalExpression) == 0:
+        return ""
+
+    if len(LogicalExpression) == 1:
+         if LogicalExpression[0] in Interpretation:
+                return Interpretation[LogicalExpression[0]]
+         if  LogicalExpression == ['true']:
+                return 'true'
+         if LogicalExpression ==  ['false']:
+                return 'false'
+
+    if len(LogicalExpression) == 2:
+        if isinstance(LogicalExpression[0], str):
+=======
         
     if isinstance(LogicalExpression, str):
         if LogicalExpression in Interpretation:
@@ -32,18 +56,26 @@ def interpret(LogicalExpression, Interpretation):
         if isinstance(LogicalExpression[0], str):
             if LogicalExpression[0] in Interpretation:
                 return Interpretation[LogicalExpression[0]] 
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
             if  LogicalExpression == ['NOT', 'true']:
                 return 'false'
             if LogicalExpression ==  ['NOT', 'false']:
                 return 'true'
         return interpret([LogicalExpression[0]] + [interpret(LogicalExpression[1], Interpretation)], Interpretation)
 
+<<<<<<< HEAD
+    if len(LogicalExpression) == 3:
+                 
+        if [interpret(LogicalExpression[0],Interpretation), LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "OR", "true"]:
+             return "true"
+=======
 
     if len(LogicalExpression) == 3: 
         if [interpret(LogicalExpression[0], Interpretation), LogicalExpression[1], interpret(LogicalExpression[2], Interpretation)] == ["true" "OR" "true"]:
             return "true"
         if [interpret(LogicalExpression[0],Interpretation), LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "OR", "true"]:
             return "true"
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
         if [interpret(LogicalExpression[0],Interpretation), LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "OR", "false"]:
             return "true"
         if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "OR", "true"]:
@@ -57,6 +89,9 @@ def interpret(LogicalExpression, Interpretation):
         if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "AND", "true"]:
             return "false"
         if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "AND", "false"]: 
+<<<<<<< HEAD
+            return 'false'
+=======
             return 'false'       
 
 
@@ -65,3 +100,4 @@ def interpret(LogicalExpression, Interpretation):
 
  
 
+>>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6

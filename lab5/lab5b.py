a7cc74d12e2a72ba6a959118b87e82970bd02b8b
Daniel Alchasov <danal315@student.liu.se>
2022-11-01 11:53:47 +0000

    Replace lab5b.py

--- lab5/lab5b.py
+++ lab5/lab5b.py
@@ -4,7 +4,6 @@ import random
 from typing import Type
 import cv2
 import math
-from cv2 import imread
 import numpy
 
 
@@ -19,10 +18,41 @@ def pixel_constraint(hlow, hhigh, slow, shigh, vlow, vhigh):
             return 0
     return is_what
 
+def pixel_constrait_test(): # what should i do with negatives 
+    # eftersom HSV kan endast vara mellan 0-255 blir det inte speciellt många extremfall förutom om typ high 
+
+    condition1 = pixel_constraint(0,0,0,0,0,0)
+    assert condition1((0,0,0)) == 1
+    condition1 = pixel_constraint(5, -5,5,-5,5,-5)
+    assert condition1((5,5,5)) == 0
+    condition1 = pixel_constraint(50, 150,50,150,50,150)
+    assert condition1((63,23,75)) == 0
+    assert condition1((70,85,124)) == 1
+    assert condition1(([123,2],50, 10)) == 0 
+
+
+
+def multiply_tuple(tpl, mult):
+    """Return a tuple where all elements are scaled by factor 'mult'.
+
+    (a,b,c) * k = (a*k, b*k, c*k)
+    """
+    return tuple(map(lambda x: x*mult, tpl))
+
+
+def add_tuples(tpl1, tpl2):
+    """
+    Return a the element-wise sum of tpl1 and tpl2.
+
+    (a,b,c) + (d,e,f) = (a+d, b+e, c+f)
+    """
+    return tuple(map(lambda t1, t2: t1+t2, tpl1, tpl2))
+
+
 
 def cvimg_to_list(filename):
     list_of_colors = []
-    image =  filename
+    image = filename
     for i in range(image.shape[0]):
         for q in range(image.shape[1]):
             list_of_colors.append(tuple(image[i,q]))
@@ -47,43 +77,71 @@ def rgblist_to_cvimg(lst, height, width):
 
 def generator_from_image(image_as_list):
 
-    """Gives the BGR value for a pixel after it reads the list of it"""
+    """Gives the BGR value for a given pixels index"""
+
     def generator(index):
-        return image_as_list[index]
+        try:
+            if isinstance(index, int): 
+                return image_as_list[index]
+            else: 
+                raise IndexError
+        except IndexError:
+            return "Given index is out of bounds"
+        except TypeError:
+            return "Given value is of the wrong type"
     return generator
 
- 
-def combine_images(mask,  mask_function, image_generator1, image_generator2):  # just brute force it ig
+def generator_from_image_test():
+    test = generator_from_image([(0,0,0), (0,0,0)])
+    assert test(0) == (0,0,0)
+    test = generator_from_image([(128,128,128), (0,0,0)])
+    assert test(0) == (0,0,0)
 
+def combine_images(mask,  mask_function, image_generator1, image_generator2): 
 
     """Mask, mask_function is the conditioning to figure out if img1 or img2 is supposed to be used. 
     Creates a list with new values for each pixel from img1 and img2 based on a mask """
 
     list_colors = []
-    conditon_image = []
     condition = gradient_condition(mask)
-    img1 = [image_generator1(i) for i in range(len(mask))]
-    img2 = [image_generator2(i) for i in range(len(mask))]
-    for A in range(273280):
-        if condition(A) == 1:
-            list_colors.append(img1[A])
-        elif condition(A) == 0:
-            list_colors.append(img2[A])
+    try:
+
+            img1 = [image_generator1(i) for i in range(len(mask))] # behöver inte köra på resten av koden då om detta fungerar inte då körs inte resterande. 
+            img2 = [image_generator2(i) for i in range(len(mask))] # problem när den är out of bounds 
+          # what to do if a string gets put in :()
+    except IndexError:
+        return "Image generators index is larger than len(mask)"
+    except TypeError:
+        return "Image_generator has non tuples."
+    else:
+        if not (len(mask) == len(img1) or len(mask) == len(img2)):
+            raise IndexError
         else:
-            list_of_colors.append(img1[A] * condition(A) + img2 *condition[A])
-    return list_colors
+            for A in range(len(mask)):
+                if mask_function(A) == 1:
+                    list_colors.append(img1[A])
+                elif mask_function(A) == 0:
+                    list_colors.append(img2[A])
+                else:
+                    list_colors.append(add_tuples(multiply_tuple(img1[A], condition(A)), multiply_tuple(img2[A], (1-condition(A)))))
+        return list_colors
 
 
-def combine_images_Original(mask,  mask_function, image_generator1, image_generator2): 
-    list_colors = []
-    img1 = [image_generator1(i) for i in range(len(mask))]
-    img2 = [image_generator2(i) for i in range(len(mask)) ]
-    for A in range(len(mask)):
-        if  mask_function(mask[A]) == 1:
-            list_colors.append(img1[A])            
-        else:
-            list_colors.append(img2[A])
-    return list_colors
+def combine_images_test(): # test base case, and then extreme 
+
+    condition = gradient_condition([(1,3,2)])
+    gen1 = generator_from_image([(23,21,52)])
+    gen2 = generator_from_image([(42,32,64)])
+    mask = [(1,3,2)]
+
+    
+    assert combine_images(mask, condition, gen1, gen2) == [(41.809999999999995, 31.89,63.88)]
+
+    mask = [(255,255,255)]
+    conditiona = gradient_condition([(128,128,128), (0,0,0)])
+    gen1 = generator_from_image([(255,172,255), (0,0,0)])
+    gen2 = generator_from_image([(0,80, 53), (0,0,0)])
+    return combine_images(mask, conditiona, gen1, gen2) #== [(255,172,255), (0,0,0)]
 
 
 def greyscale_list_to_cvimg(lst, height, width):
@@ -100,41 +158,17 @@ def greyscale_list_to_cvimg(lst, height, width):
 def gradient_condition(mask):
 
     """Returns what conditon a pixel is on. If its BGR value is 255,255,255 it returns 1 otherwise between 1-0"""
+
     def condition(index):
-        try: 
+        try:
             if isinstance(mask[index], tuple) and len(mask[index]) == 3:
-                return round((sum(mask[index])/765),2)
+                return round((sum(mask[index])/765),2) 
             else:
                 raise TypeError
         except IndexError:
-            return "Index bigger thaan len(mask)"
+            return "Given index is larger than len(mask)"
         except TypeError:
-            return "Index must be an integer or a slice int"
+            return "Given 'index' must be an integer or instance must be a tuple"
     return condition
-   
 
-# Läs in en bild
-plane_img = cv2.imread("plane.jpg")
-flower_image = cv2.imread('flowers.jpg')
-gradient_image = cv2.imread('gradient.jpg')
-# Skapa ett filter som identifierar himlen
-condition = pixel_constraint(100, 150, 50, 200, 100, 255)
-
-# Omvandla originalbilden till en lista med HSV-färger
-gradient_image_list = cvimg_to_list(cv2.cvtColor(gradient_image, cv2.COLOR_BGR2HSV))
-plane_img_list = cvimg_to_list(plane_img)
-flower_img_list = cvimg_to_list(flower_image)
-condition = gradient_condition(gradient_image_list)
-
-# Skapa en generator för den inlästa bilden
-generator1 = generator_from_image(flower_img_list)
-generator2 = generator_from_image(plane_img_list)
-mask = gradient_image_list
-
-# Kombinera de två bilderna till en, alltså använd himmelsfiltret som mask
-result = combine_images(mask, condition, generator1, generator2)
-
-# Omvandla resultatet till en riktig bild och visa upp den
-new_img = rgblist_to_cvimg(result, plane_img.shape[0], plane_img.shape[1])
-cv2.imshow('Final image', new_img)
-cv2.waitKey(0)
+print(combine_images_test())

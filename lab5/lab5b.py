b18aacde04637bf9ccd365d8d2c415623fe30770
Daniel Alchasov <danal315@student.liu.se>
2022-11-03 10:31:50 +0000

    Replace lab5b.py

--- lab5/lab5b.py
+++ lab5/lab5b.py
@@ -7,6 +7,7 @@ import numpy
 
 
 def pixel_constraint(hlow, hhigh, slow, shigh, vlow, vhigh):
+
     """Checks if a pixels HSV values matches with a set conditions and returns 1 or 0 depending if it does or not"""
 
     def pixel_condition(hsv):
@@ -36,6 +37,7 @@ def add_tuples(tpl1, tpl2):
 
 
 def cvimg_to_list(filename):
+
     """Returns a list of tuples of a images RGB values"""
 
     list_of_colors = []
@@ -109,3 +111,28 @@ def gradient_condition(mask):
             return index/255
         
     return condition
+
+# Läs in en bild
+plane_img = cv2.imread("plane.jpg")
+flower_image = cv2.imread('flowers.jpg')
+gradient_image = cv2.imread('gradient.jpg')
+
+# Omvandla originalbilden till en lista med HSV-färger
+gradient_image_list = cvimg_to_list((gradient_image))
+plane_img_list = cvimg_to_list(plane_img)
+flower_img_list = cvimg_to_list(flower_image)
+
+# Skapa en generator för den inlästa bilden
+generator1 = generator_from_image(flower_img_list)
+generator2 = generator_from_image(plane_img_list)
+
+mask = gradient_image_list
+mask_function = gradient_condition(gradient_image_list)
+
+# Kombinera de två bilderna till en, alltså använd himmelsfiltret som mask
+result = combine_images(mask, mask_function, generator1, generator2)
+
+# Omvandla resultatet till en riktig bild och visa upp den
+new_img = rgblist_to_cvimg(result, plane_img.shape[0], plane_img.shape[1])
+cv2.imshow('Final image', new_img)
+cv2.waitKey(0)

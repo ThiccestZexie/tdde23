5a62f05b29c0029b252323f99d9ee845d8743a24
Daniel Alchasov <danal315@student.liu.se>
2022-11-03 14:10:15 +0000

    Replace lab4b.py

--- lab4/lab4b.py
+++ lab4/lab4b.py
@@ -1,103 +1,60 @@
-<<<<<<< HEAD
-def interpret(LogicalExpression, Interpretation):
-=======
-from re import I
-
-
-def interpret(LogicalExpression, Interpretation): 
-
-    if len(Interpretation) == 0:
-            return 'true'
->>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
-    if LogicalExpression == 'true':
-        return LogicalExpression
-    if LogicalExpression == 'false':
-        return LogicalExpression
-<<<<<<< HEAD
-    if isinstance(LogicalExpression, str):
-        if LogicalExpression in Interpretation:
-            return Interpretation[LogicalExpression]
-        return LogicalExpression
-
-    if len(LogicalExpression) == 0:
-        return ""
-
-    if len(LogicalExpression) == 1:
-         if LogicalExpression[0] in Interpretation:
-                return Interpretation[LogicalExpression[0]]
-         if  LogicalExpression == ['true']:
-                return 'true'
-         if LogicalExpression ==  ['false']:
-                return 'false'
-
-    if len(LogicalExpression) == 2:
-        if isinstance(LogicalExpression[0], str):
-=======
-        
-    if isinstance(LogicalExpression, str):
-        if LogicalExpression in Interpretation:
-            return Interpretation[LogicalExpression]
-        return LogicalExpression 
+def interpret(logical_expression, interpretation): 
     
-    
+    if logical_expression == 'true':
+        return logical_expression
+    if logical_expression == 'false':
+        return logical_expression
+    if isinstance(logical_expression, str):
+        if logical_expression in interpretation:
+            return interpretation[logical_expression]
+        return logical_expression
 
-    if len(LogicalExpression) == 0:
-        return "invalid input"
-
-    if len(LogicalExpression) == 1:
-            if LogicalExpression[0] in Interpretation:
-                return Interpretation[LogicalExpression[0]] # for example will return Cat_asleep...
-            if  LogicalExpression == 'true':
-                return 'true'
-            if LogicalExpression ==  'false':
+    if len(logical_expression) == 2:
+        if isinstance(logical_expression[0], str):
+            if  logical_expression == ['NOT', 'true']:
                 return 'false'
-    interpret(LogicalExpression[0], Interpretation) # checks if first one is true or not 
-    if len(LogicalExpression) == 2:
-        if isinstance(LogicalExpression[0], str):
-            if LogicalExpression[0] in Interpretation:
-                return Interpretation[LogicalExpression[0]] 
->>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
-            if  LogicalExpression == ['NOT', 'true']:
-                return 'false'
-            if LogicalExpression ==  ['NOT', 'false']:
+            if logical_expression ==  ['NOT', 'false']:
                 return 'true'
-        return interpret([LogicalExpression[0]] + [interpret(LogicalExpression[1], Interpretation)], Interpretation)
+        return interpret([logical_expression[0]] + [interpret(logical_expression[1], interpretation)], interpretation)
 
-<<<<<<< HEAD
-    if len(LogicalExpression) == 3:
-                 
-        if [interpret(LogicalExpression[0],Interpretation), LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "OR", "true"]:
-             return "true"
-=======
-
-    if len(LogicalExpression) == 3: 
-        if [interpret(LogicalExpression[0], Interpretation), LogicalExpression[1], interpret(LogicalExpression[2], Interpretation)] == ["true" "OR" "true"]:
-            return "true"
-        if [interpret(LogicalExpression[0],Interpretation), LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "OR", "true"]:
-            return "true"
->>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
-        if [interpret(LogicalExpression[0],Interpretation), LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "OR", "false"]:
-            return "true"
-        if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "OR", "true"]:
-            return "true"
-        if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "OR", "false"]:
-            return "false"
-        if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "AND", "true"]:
-            return "true"
-        if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["true", "AND", "false"]:
-            return "false"
-        if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "AND", "true"]:
-            return "false"
-        if [interpret(LogicalExpression[0],Interpretation) ,LogicalExpression[1], interpret(LogicalExpression[2],Interpretation)] == ["false", "AND", "false"]: 
-<<<<<<< HEAD
-            return 'false'
-=======
-            return 'false'       
+    if len(logical_expression) == 3:
+                    if [interpret(logical_expression[0], interpretation), logical_expression[1], interpret(logical_expression[2], interpretation)] == ["true" "OR" "true"]:
+                        return "true"
+                    if [interpret(logical_expression[0],interpretation), logical_expression[1], interpret(logical_expression[2],interpretation)] == ["true", "OR", "true"]:
+                        return "true"
+                    if [interpret(logical_expression[0],interpretation), logical_expression[1], interpret(logical_expression[2],interpretation)] == ["true", "OR", "false"]:
+                        return "true"
+                    if [interpret(logical_expression[0],interpretation) ,logical_expression[1], interpret(logical_expression[2],interpretation)] == ["false", "OR", "true"]:
+                        return "true"
+                    if [interpret(logical_expression[0],interpretation) ,logical_expression[1], interpret(logical_expression[2],interpretation)] == ["false", "OR", "false"]:
+                        return "false"
+                    if [interpret(logical_expression[0],interpretation) ,logical_expression[1], interpret(logical_expression[2],interpretation)] == ["true", "AND", "true"]:
+                        return "true"
+                    if [interpret(logical_expression[0],interpretation) ,logical_expression[1], interpret(logical_expression[2],interpretation)] == ["true", "AND", "false"]:
+                        return "false"
+                    if [interpret(logical_expression[0],interpretation) ,logical_expression[1], interpret(logical_expression[2],interpretation)] == ["false", "AND", "true"]:
+                        return "false"
+                    if [interpret(logical_expression[0],interpretation) ,logical_expression[1], interpret(logical_expression[2],interpretation)] == ["false", "AND", "false"]: 
+                        return 'false'
 
 
-       
+def test():
+    print(interpret(["door_open", "AND", "cat_gone"], {"door_open" : "false", "cat_gone" : "true", "cat_asleep" : "true"} )) # 'false'
+
+    print(interpret(["cat_asleep", "OR", ["NOT", "cat_gone"]],{"door_open" : "false", "cat_gone" : "true", "cat_asleep" : "true"})) #true
+
+    print(interpret(["true", "OR", "true"], {})) #'true'
+
+    print(interpret("cat_gone", {"door_open": "false", "cat_gone": "true"})) #true
+
+    print(interpret(["NOT", ["NOT", ["NOT", ["cat_asleep", "OR", ["NOT", "cat_asleep"]]]]],{"cat_asleep": "false"})) #false
+
+    print(interpret(["NOT", "AND", "true"], {"NOT":"true"})) #true
+
+    print(interpret(["NOT", "AND"], {"AND": "false"})) #true
+
+    print(interpret(["NOT", "NOT"], {"NOT": "false"})) #true
 
 
- 
+test()
 
->>>>>>> f3c81ce51741594f46e31b82a03932d9b2cfc1d6
